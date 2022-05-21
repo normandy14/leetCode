5d91fc3921c7b3934ad9a21026acc2d2e4bd424e
normandy14 <sturdyprogrammer@gmail.com>
2022-05-21 16:23:09 -0400

    Add doc to longestCommonPrefix

--- longestCommonPrefix/longestCommon.cpp
+++ longestCommonPrefix/longestCommon.cpp
@@ -22,7 +22,7 @@ vector<string> getAllSubStrings(string smallestWord) {
   vector <string> words;              // a list to hold the split substrings
   words.push_back(s);                 // the full word is treated as exception
   
-  int i = 0;                          // manipulate the i and j index
+  int i = 0;                          // manipulate the i and j indexes
   int j = s.size();                   // to create substrigs
   
   /*
@@ -31,39 +31,29 @@ vector<string> getAllSubStrings(string smallestWord) {
   */
   
   
-  while (j > 1) {                     //
+  while (j > 1) {                     // the runtime error is probably here
     
     j -= 1;
     
     for (i; i + j <= s.size(); i++) {
-      string sub = s.substr(i, j);
+      string sub = s.substr(i, j);    // manipulate i and j indexes to get substrings
       // cout << sub << endl;
-      words.push_back(sub);
+      words.push_back(sub);           // record each substring in the vector
     }
     i = 0;
   }
-  /*
-  for (string w: words) {
-    cout << w << endl;
-  }
-  */
-  return words;
+  
+  return words;                       // return that vector
 }
 
 string commonPrefix(vector<string> v, vector<string> subV) {
-  // string prefix = subV[0];
-  // cout << "prefix: " << prefix;
-  bool flag = true;
   for (string p: subV) {
-    // cout << "p: " << p << endl;
     for (string w: v) {
-      // cout << "w: " << w << endl;
-      if (w.rfind(p, 0) == -1) {
+      if (w.rfind(p, 0) == -1) {      // ignore when a substring is not found in this word in vector
         flag = false;
       }
     }
-    if (flag == true) {
-      // cout << "answer is: " << p << endl;
+    if (flag == true) {               // return the first time substring is in all words in vector
       return p;
     }
     flag = true;
@@ -71,12 +61,13 @@ string commonPrefix(vector<string> v, vector<string> subV) {
   return "";
 }
 
-string Solution::longestCommonPrefix(vector<string>& strs) {
-  sort(strs.begin(), strs.end(), compareFn);    // sort by length of word
+string Solution::longestCommonPrefix(vector<string>& strs) {  // Sets up problem
+  sort(strs.begin(), strs.end(), compareFn);                  // sort by length of word
   vector<string> subWords = getAllSubStrings(strs[0]);
   string s = commonPrefix(strs, subWords);
   return s;
 }
+  
 int main() {
   Solution sol;                                         // create class in scope
   vector<string> strs = { "flower","flow","flight" };   // sorted test strings
